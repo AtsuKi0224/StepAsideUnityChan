16c6da4b2cc0d03f7382f2152f6ba1e7aad54ef3
AtsiKi <a.atsuki0618@gmail.com>
2018-11-10 02:35:41 +0900

    StepAsideUnityChanプロジェクト　課題のコミット
    画面外に出たアイテムを削除

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start () {
+        this.unitychan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.transform.position.z + 5 <= unitychan.transform.position.z) {
+            Destroy(this.gameObject);
+        }
+		
+	}
+}
